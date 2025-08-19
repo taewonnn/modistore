c710b1f4b4ca68a728b38b4365ad8d0aada5d358
eden <eden@rainbow.co.kr>
2025-08-19 10:00:00 +0900

    add: 구조

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -32,3 +33,4 @@ export default function RootLayout({
     </html>
   );
 }
+  
